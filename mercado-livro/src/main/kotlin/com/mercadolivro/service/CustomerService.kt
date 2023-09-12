bfd09ae72d01e2103a4483181f03b62876c0638e
jonathask <jonathas.knoth@grupoboticario.com.br>
2023-09-12 17:09:03 -0300

    45. Implementando o nosso Repository

--- mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,38 +1,43 @@
 package com.mercadolivro.service
 
 import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.repository.CustomerRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class CustomerService {
-    private val customers = mutableListOf<CustomerModel>()
+class CustomerService(
+        val customerRepository: CustomerRepository
+) {
 
     fun getAll(name: String?): List<CustomerModel> {
+        //println("Valor de nome: $name")
         name?.let {
-            return customers.filter { it.name.contains(name, ignoreCase = true) }
+            return customerRepository.findByNameContaining(it)
         }
-        return customers
+
+        return customerRepository.findAll().toList()
     }
 
-    fun createCustomer(request: CustomerModel) {
-        val id = if (customers.isEmpty()) 1 else customers.last().id!!.toInt() + 1
-        request.id = id
-        customers.add(CustomerModel(id, request.name, request.email))
+    fun createCustomer(customer: CustomerModel) {
+        customerRepository.save(customer)
     }
 
     fun getCustomer(id: Int): CustomerModel? {
-        val customer: CustomerModel? = customers.find { it -> it.id == id }
-        return customer;
+        return customerRepository.findByIdOrNull(id)
     }
 
     fun update(customer: CustomerModel) {
-        customers.filter { it.id == customer.id }.first().let {
-            it.name = customer.name
-            it.email = customer.email
-        }
+       if(!customerRepository.existsById(customer.id!!)){
+           throw Exception("Registro não existe");
+       }
+        customerRepository.save(customer)
     }
 
     fun deletar(id: Int) {
-        customers.removeIf { it.id == id }
+        if(!customerRepository.existsById(id)){
+            throw Exception("Registro não existe");
+        }
+        customerRepository.deleteById(id)
     }
 }
